71b911ef77185df74ecea9e125e8c6037409402a
Christian Gonzalez <cgz@ad.luxyachts.com>
2016-04-28 09:26:30 -0400

    test

---
+++ routes/videos.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/testDb');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function(){
+    console.log('connected using mongoose!');
+});
+
+var Schema = mongoose.Schema;
+var videoSchema = new Schema({
+    title: String,
+    description: String
+});
+var Video = mongoose.model('Video', videoSchema);
+
+router.get('/', function(req, res){
+    Video.find({}).find(function(err, videos){
+        console.log(videos);
+        var results = [];
+        res.send(results);
+    });
+});
+// var monk = require('monk');
+// var db = monk('localhost:27017/testDb');
+
+// router.get('/', function(req, res) {
+//     var collection = db.get('videos');
+//     collection.find({}, function(err, videos){
+//         if (err) throw err;
+//       	res.json(videos);
+//     });
+// });
+
+// router.post('/', function(req, res){
+//     var collection = db.get('videos');
+//     collection.insert({
+//         title: req.body.title,
+//         description: req.body.description
+//     }, function(err, video){
+//         if (err) throw err;
+//         res.json(video);
+//     });
+// });
+
+module.exports = router;
