5899ffcf9fdbdc436e7f4e31c14f55ea3b0a2f87
Christian Gonzalez <cgonzalez@CCIITDEV.local>
2018-04-16 15:32:10 -0400

    new commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var videos = require('./routes/videos');
-
+// some change to commit
 var app = express();
 
 // view engine setup
