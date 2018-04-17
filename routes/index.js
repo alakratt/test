760d41121761670cf87d157cdf3e51b00cee71ac
Christian <alakratt@gmail.com>
2018-04-17 15:02:42 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-// new comment!
+// new comment! 2
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
