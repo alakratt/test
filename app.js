aa2d2c806e99623e205b2a9b054f5ac75dc24fb2
Christian Gonzalez <cgonzalez@CCIITDEV.local>
2018-04-17 14:06:37 -0400

    yet another commit

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// on 4/172018 I made this comment for, you guessed it, another commit test
 
 app.use('/', routes);
 app.use('/users', users);
