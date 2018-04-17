1909c067daa8ba5b602ae77c48324ca7bba3b4dd
Christian <alakratt@gmail.com>
2018-04-17 15:08:21 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+// you get users from here
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
