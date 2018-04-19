7ff1c0e177d94a0b4a7897a4bb0be8bfd6d40ac1
Christian <alakratt@gmail.com>
2018-04-18 21:59:25 -0400

    testing from atom ide

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 // new comment! from my laptop
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 // bla bla bla, just another test from the CLI
 module.exports = router;
