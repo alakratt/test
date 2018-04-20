ca674e042e18b542a0659065b5162065d69e4bbe
Christian <alakratt@gmail.com>
2018-04-20 16:18:24 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 // some new comment
-
+// new comment 4-20
 // it's 420 maaaaaaaaan
 /* GET users listing. */
 router.get('/', function(req, res, next) {
