6030b0eec9491f24623dd239d78361f97bd69924
Christian Gonzalez <cgonzalez@CCIITDEV.local>
2018-04-20 16:56:06 -0400

    testing

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-// some new comment
+
 // new comment 4-20
 // it's 420 maaaaaaaaan
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-// bassasa
+
 module.exports = router;
