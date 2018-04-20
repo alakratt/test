df0c8f57811e88c779a4cb403e95cf54b71a24cc
Chris <alakratt@gmail.com>
2018-04-20 16:58:22 -0400

    cleanup

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // new comment 4-20
 // it's 420 maaaaaaaaan
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
