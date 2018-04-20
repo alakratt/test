208195a36c21f64a9ffbd16d3e2d78d4098c75db
Christian Gonzalez <cgonzalez@CCIITDEV.local>
2018-04-20 12:21:16 -0400

    testing other branches

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 // some new comment
 // a new comment on 4-19-2018
-// a new comment on 4-19-2018 v2
+// a new comment on 4-19
+// it's 420 maaaaaaaaan
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
